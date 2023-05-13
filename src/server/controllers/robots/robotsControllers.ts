1dee53f432f010468d329010765a437b827566d1
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-13 18:07:55 +0200

    Fix getRobots to return void instead of promise

--- src/server/controllers/robots/robotsControllers.ts
+++ src/server/controllers/robots/robotsControllers.ts
@@ -1,16 +1,14 @@
 import { type NextFunction, type Request, type Response } from "express";
 import Robot from "../../../database/models/Robots.js";
 
-export const getRobots = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const robots = await Robot.find().exec();
+export const getRobots = (req: Request, res: Response, next: NextFunction) => {
+  (async () => {
+    try {
+      const robots = await Robot.find().exec();
 
-    res.status(200).json({ robots });
-  } catch (error: unknown) {
-    next(error);
-  }
+      res.status(200).json({ robots });
+    } catch (error: unknown) {
+      next(error);
+    }
+  })();
 };
